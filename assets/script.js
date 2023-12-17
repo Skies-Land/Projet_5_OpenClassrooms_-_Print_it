d62a44bec522b1361f2f221a1c2afdeca100487a
Jonathan Araldi <jonathan.araldi@outlook.com>
2023-12-17 21:49:48 +0100

    Modification des flèches de navigation pour le HTML. Modification des bullet points afin qu'ils soient cliquables (CSS) et apportent un changement d'image (JavaScript).

--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,41 @@
 /*=============== TABLEAU D'IMAGES ===============*/
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		image:"slide1.jpg",
+		tagLine:"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		image:"slide2.jpg",
+		tagLine:"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		image:"slide3.jpg",
+		tagLine:"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		image:"slide4.png",
+		tagLine:"Autocollants <span>avec découpe laser sur mesure</span>"
 	},
 ];
 
 /*=============== VARIABLES ===============*/
+//#region
+
 	/* Bullets points */
 	const dots = document.querySelector(".dots");
 
-	/* Flèche de navigation du slider */
-	const arrowRight = document.querySelector(".fa-arrow-right");
-	const arrowLeft = document.querySelector(".fa-arrow-left");
+	/* Flèche de navigation du carrousel */
+	const arrowRight = document.querySelector(".arrow_right");
+	const arrowLeft = document.querySelector(".arrow_left");
 
-	/* Selection des images et des textes à afficher */
+	/* Images et des textes à afficher */
 	const img = document.querySelector(".banner-img");
 	const tagLine = document.querySelector("#banner p");
 
 
 	let index = 0;
 
-
-/*=============== FONCTION PRINCIPALE ===============*/
-function main() {
-	displayDots();
-	slideRight();
-	slideLeft();
-}
-main();
+//#endregion
 
 /*=============== AFFICHAGE DES DOTS ===============*/
 function displayDots() {
@@ -48,12 +43,36 @@ function displayDots() {
 		const dot = document.createElement("div");
 		dot.classList.add("dot");
 		dots.appendChild(dot);
+
+		/* Ajouter un "écouteur d'événements" pour chaque point indicateur */
+		dot.addEventListener("click", () => {
+
+            /* Mets à jour l'index et change l'image du carrousel */
+            index = i;
+            updateCarousel();
+        });
+
 		if (i == index) {
-			/* ⬇ change la couleur du "dot" avec la class CSS
+			/* Change la couleur du "dot" avec la class CSS
 			".dot_selected" concernant la slide sélectionné */
 			dot.classList.add("dot_selected");
 		}
 	}
+}
+
+/* Fonction pour mettre à jour le carrousel en fonction de l'index actuel */
+function updateCarousel() {
+    const selectDots = document.querySelectorAll(".dots .dot");
+    
+    /* Supprime la classe "dot_selected" du point actuellement sélectionné */
+    selectDots[index].classList.remove("dot_selected");
+
+    /* Mettre à jour l'image et le texte du carrousel */
+    img.src = "./assets/images/slideshow/" + slides[index].image;
+    tagLine.innerHTML = slides[index].tagLine;
+
+    /* Ajouter la classe "dot_selected" au nouveau point sélectionné */
+    selectDots[index].classList.add("dot_selected");
 }
 
 /*=============== EVENT LISTENER ===============*/
@@ -76,10 +95,8 @@ function displayDots() {
 				index = 0;
 			}
 
-			/* changement d'image au déroulement du slider */
+			/* Changement d'image et de texte au déroulement du carrousel */
 			img.src = "./assets/images/slideshow/" + slides[index].image;
-
-			/* changement le texte au déroulement du slider */
 			tagLine.innerHTML = slides[index].tagLine;
 
 			/* Dans le tableau "selectDots" prendre l'index actuelle
@@ -102,16 +119,14 @@ function displayDots() {
 			
 			index--;
 
-			/* ⬇ La condition, si la valeur du tableau "slides" est égale à 0
+			/* La condition, si la valeur du tableau "slides" est égale à 0
 			alors le carrousel d'image repart à l'image précédente */
 			if (index < 0) {
 				index = slides.length - 1;
 			}
 
-			/* changement d'image au déroulement du slider */
+			/* Changement d'image et de texte au déroulement du carrousel */
 			img.src = "./assets/images/slideshow/" + slides[index].image;
-
-			/* changement le texte au déroulement du slider */
 			tagLine.innerHTML = slides[index].tagLine;
 
 			/* Dans le tableau "selectDots" prendre l'index actuelle
@@ -119,3 +134,12 @@ function displayDots() {
 			selectDots[index].classList.add("dot_selected");
 		});
 	}
+
+
+/*=============== FONCTION PRINCIPALE ===============*/
+function main() {
+	displayDots();
+	slideRight();
+	slideLeft();
+}
+main();	
