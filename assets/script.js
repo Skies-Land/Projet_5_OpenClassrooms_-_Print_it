84af74d3f10a112a2eacc949f4c8cad1307a6820
Jonathan Araldi <jonathan.araldi@outlook.com>
2023-12-18 21:16:33 +0100

    Modification du code JavaScript avec ajout de commentaires

--- assets/script.js
+++ assets/script.js
@@ -1,145 +1,144 @@
 /*=============== TABLEAU D'IMAGES ===============*/
 const slides = [
-	{
-		image:"slide1.jpg",
-		tagLine:"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		image:"slide2.jpg",
-		tagLine:"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		image:"slide3.jpg",
-		tagLine:"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		image:"slide4.png",
-		tagLine:"Autocollants <span>avec découpe laser sur mesure</span>"
-	},
+    {
+        image: "slide1.jpg",
+        tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+    },
+    {
+        image: "slide2.jpg",
+        tagLine: "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+    },
+    {
+        image: "slide3.jpg",
+        tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+    },
+    {
+        image: "slide4.png",
+        tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+    },
 ];
 
 /*=============== VARIABLES ===============*/
 //#region
 
-	/* Bullets points */
-	const dots = document.querySelector(".dots");
+	/* Sélectionne l'élément du DOM avec la classe "dots" et le stocke dans la variable dotsContainer */
+	const dotsContainer = document.querySelector(".dots");
 
-	/* Flèche de navigation du carrousel */
+	/* Sélectionne les éléments du DOM avec les classes "arrow_right" et "arrow_left" 
+	et les stocke dans les variables arrowRight et arrowLeft */
 	const arrowRight = document.querySelector(".arrow_right");
 	const arrowLeft = document.querySelector(".arrow_left");
 
-	/* Images et des textes à afficher */
+	/* Sélectionne les éléments du DOM ayant les classes 
+	"banner-img" et "#banner p" et les stocke dans les variables img et tagLine */
 	const img = document.querySelector(".banner-img");
 	const tagLine = document.querySelector("#banner p");
 
-
+	/* Initialise l'index à 0, représentant la position actuelle dans le carrousel. */
 	let index = 0;
 
 //#endregion
 
-/*=============== AFFICHAGE DES DOTS ===============*/
-function displayDots() {
-	for (let i = 0; i < slides.length; i++) {
+/*=============== BULLET POINTS ===============*/
+	/* Fonction pour créer un bullet point dans le carrousel */
+	function createDot(i) {
+
+		/* Crée un élément div dans le DOM et lui ajoute la classe "dot" */
 		const dot = document.createElement("div");
 		dot.classList.add("dot");
-		dots.appendChild(dot);
 
-		/* Ajouter un "écouteur d'événements" pour chaque point indicateur */
+		/* Ajoute le bullet point au conteneur de points (dotsContainer) */
+		dotsContainer.appendChild(dot);
+
+		/* Ajoute un Event Listener au clic sur le bullet point, 
+		appelant la fonction updateCarousel avec l'index du bullet point en argument */
 		dot.addEventListener("click", () => {
+			updateCarousel(i);
+		});
 
-            /* Mets à jour l'index et change l'image du carrousel */
-            index = i;
-            updateCarousel();
-        });
-
-		if (i == index) {
-			/* Change la couleur du "dot" avec la class CSS
-			".dot_selected" concernant la slide sélectionné */
+		/* CONDITION : si le bullet point créé correspond à l'index actuel du carrousel, 
+		ajoute la classe "dot_selected" afin qui soit en surbrillance. */
+		if (i === index) {
 			dot.classList.add("dot_selected");
 		}
 	}
-}
 
-/* Fonction pour mettre à jour le carrousel en fonction de l'index actuel */
-function updateCarousel() {
-    const selectDots = document.querySelectorAll(".dots .dot");
-    
-    /* Supprime la classe "dot_selected" du point actuellement sélectionné */
-    selectDots[index].classList.remove("dot_selected");
+	/* Fonction permettant d'afficher les bullet points du carrousel */
+	function displayDots() {
 
-    /* Mettre à jour l'image et le texte du carrousel */
-    img.src = "./assets/images/slideshow/" + slides[index].image;
-    tagLine.innerHTML = slides[index].tagLine;
+		/* Parcours tous les slides et crée un bullet point pour chaque image */
+		for (let i = 0; i < slides.length; i++) {
+			createDot(i);
+		}
+	}
 
-    /* Ajouter la classe "dot_selected" au nouveau point sélectionné */
-    selectDots[index].classList.add("dot_selected");
-}
+/*=============== CARROUSEL ===============*/
+	/* Fonction permettant de mettre à jour le carrousel avec l'image 
+	et le texte correspondant à un index donné */
+	function updateCarousel(i) {
+		
+		/* Sélectionne tous les bullet points du carrousel */
+		const selectDots = document.querySelectorAll(".dots .dot");
 
-/*=============== EVENT LISTENER ===============*/
-	/*=============== SLIDE AU CLIQUE DROIT ===============*/
-	function slideRight(){
+		/* Retire la classe "dot_selected" du point actuel */
+		selectDots[index].classList.remove("dot_selected");
+
+		/* Mise à jour de l'index actuel */
+		index = i;
+
+		/* Met à jour l'image et le texte du carrousel en fonction du nouvel index */
+		img.src = "./assets/images/slideshow/" + slides[index].image;
+		tagLine.innerHTML = slides[index].tagLine;
+
+		/* Ajoute la classe "dot_selected" au nouveau bullet point */
+		selectDots[index].classList.add("dot_selected");
+	}
+
+	/* Fonction permettant d'effectuer le défilement du carrousel vers la droite 
+	ou la gauche dans la direction choisi */
+	function slide(direction) {
+
+		/* Sélectionne tous les points du carrousel */
+		const selectDots = document.querySelectorAll(".dots .dot");
+
+		/* Retire la classe "dot_selected" du bullet point actuel */
+		selectDots[index].classList.remove("dot_selected");
+
+		/*  Met à jour l'index en fonction de la direction */
+		if (direction === "right") {
+			index = (index + 1) % slides.length;
+		} else {
+			index = (index - 1 + slides.length) % slides.length;
+		}
+
+		/* Met à jour l'image et le texte du carrousel en fonction du nouvel index. */
+		img.src = "./assets/images/slideshow/" + slides[index].image;
+		tagLine.innerHTML = slides[index].tagLine;
+
+		/* Ajoute la classe "dot_selected" au nouveau bullet point */
+		selectDots[index].classList.add("dot_selected");
+	}
+
+/*=============== INTERACTION AVEC LES FLECHES ===============*/
+	/* Fonctions permettant d'activer le défilement vers 
+	la droite et la gauche avec l'ajout des Event Listeners sur les flèches  */
+	function slideRight() {
 		arrowRight.addEventListener("click", () => {
-			
-			/* Initialise un tableau de dots */
-			const selectDots = document.querySelectorAll(".dots .dot");
-			
-			/* Dans le tableau "selectDots" prendre l'index actuelle
-			et enlève la class CSS ".dot_selected" */
-			selectDots[index].classList.remove("dot_selected");
-			
-			index++;
-
-			/* ⬇ La condition, si la valeur du tableau "slides" est supérieur à 4
-			alors le carrousel d'image repart à zéro */
-			if (index > slides.length - 1) {
-				index = 0;
-			}
-
-			/* Changement d'image et de texte au déroulement du carrousel */
-			img.src = "./assets/images/slideshow/" + slides[index].image;
-			tagLine.innerHTML = slides[index].tagLine;
-
-			/* Dans le tableau "selectDots" prendre l'index actuelle
-			et ajoute la class CSS ".dot_selected" */
-			selectDots[index].classList.add("dot_selected");
+			slide("right");
 		});
 	}
-	
 
-	/*=============== SLIDE AU CLIQUE GAUCHE ===============*/
-	function slideLeft(){
+	function slideLeft() {
 		arrowLeft.addEventListener("click", () => {
-			
-			/* Initialise un tableau de dots */
-			const selectDots = document.querySelectorAll(".dots .dot");
-			
-			/* Dans le tableau "selectDots" prendre l'index actuelle
-			et enlève la class CSS ".dot_selected" */
-			selectDots[index].classList.remove("dot_selected");
-			
-			index--;
-
-			/* La condition, si la valeur du tableau "slides" est égale à 0
-			alors le carrousel d'image repart à l'image précédente */
-			if (index < 0) {
-				index = slides.length - 1;
-			}
-
-			/* Changement d'image et de texte au déroulement du carrousel */
-			img.src = "./assets/images/slideshow/" + slides[index].image;
-			tagLine.innerHTML = slides[index].tagLine;
-
-			/* Dans le tableau "selectDots" prendre l'index actuelle
-			et ajoute la class CSS ".dot_selected" */
-			selectDots[index].classList.add("dot_selected");
+			slide("left");
 		});
 	}
-
 
 /*=============== FONCTION PRINCIPALE ===============*/
-function main() {
-	displayDots();
-	slideRight();
-	slideLeft();
-}
-main();	
+	function main() {
+		displayDots();
+		slideRight();
+		slideLeft();
+	}
+
+	main();	
